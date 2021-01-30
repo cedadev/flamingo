3c051ff6a96023bc37fb66f1b645171330a475c9
Jonathan Haigh <jonathanhaigh0@gmail.com>
2021-01-30 23:32:06 +0000

    updated nc content test to be parametrized

--- tests/test_wps_subset_cru_ts.py
+++ tests/test_wps_subset_cru_ts.py
@@ -5,6 +5,7 @@ from .common import get_output, PYWPS_CFG
 from flamingo.processes.wps_subset_cru_ts import SubsetCRUTS
 
 import numpy as np
+import pytest
 import xarray as xr
 import xml.etree.ElementTree as ET
 
@@ -18,9 +19,13 @@ def test_wps_subset_cru_ts(load_ceda_test_data):
     assert_response_success(resp)
     assert "meta4" in get_output(resp.xml)["output"]
 
-def test_wps_subset_cru_ts_check_nc_content(load_ceda_test_data):
+@pytest.mark.parametrize(
+    "variable,start_date,end_date,area",
+    [('wet', '1951-01-01', '2005-12-15', ['1','1','300','89'])]
+)
+def test_wps_subset_cru_ts_check_nc_content(load_ceda_test_data, variable, start_date, end_date, area):
     client = client_for(Service(processes=[SubsetCRUTS()], cfgfiles=[PYWPS_CFG]))
-    datainputs = "dataset_version=cru_ts.4.04;variable=wet;time=1951-01-01/2005-12-15;area=1,1,300,89"
+    datainputs = f"dataset_version=cru_ts.4.04;variable={variable};time={start_date}/{end_date};area={','.join(area)}"
     resp = client.get(
         f"?service=WPS&request=Execute&version=1.0.0&identifier=subset_cru_ts&datainputs={datainputs}"
     )
@@ -36,7 +41,7 @@ def test_wps_subset_cru_ts_check_nc_content(load_ceda_test_data):
     ds = xr.open_dataset(nc_output_file)
 
     data_vars = ds.data_vars
-    expected_vars = set(['time', 'wet', 'lat', 'lon'])
+    expected_vars = set(['time', variable, 'lat', 'lon'])
     found_vars = set(ds.variables.keys())
 
     assert len(data_vars) == 1
@@ -45,15 +50,15 @@ def test_wps_subset_cru_ts_check_nc_content(load_ceda_test_data):
     first_date = ds.variables['time'].values[0]
     last_date = ds.variables['time'].values[-1]
 
-    assert first_date >= np.datetime64('1951-01-01')
-    assert last_date <= np.datetime64('2005-12-15')
+    assert first_date >= np.datetime64(start_date)
+    assert last_date <= np.datetime64(end_date)
 
-    min_lat = ds.variables['lat'].values[0]
-    max_lat = ds.variables['lat'].values[-1]
     min_lon = ds.variables['lon'].values[0]
+    min_lat = ds.variables['lat'].values[0]
     max_lon = ds.variables['lon'].values[-1]
+    max_lat = ds.variables['lat'].values[-1]
 
-    assert min_lat >= 1
-    assert max_lat <= 89
-    assert min_lon >= 1
-    assert max_lon <= 300
+    assert min_lon >= int(area[0])
+    assert min_lat >= int(area[1])
+    assert max_lon <= int(area[2])
+    assert max_lat <= int(area[3])
