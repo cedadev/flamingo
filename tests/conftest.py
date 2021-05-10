a2f34182527c2c2bad5169abac0c89282cd0f1f8
Ag Stephens <ag.stephens@stfc.ac.uk>
2021-05-10 21:19:16 +0000

    updated conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,9 @@ import os
 import pytest
 import shutil
 
-from tests.common import write_roocs_cfg, MINI_CEDA_MASTER_DIR
+from git import Repo
+
+from tests.common import write_roocs_cfg, MINI_CEDA_CACHE_DIR
 
 CEDA_TEST_DATA_REPO_URL = "https://github.com/cedadev/mini-ceda-archive"
 
@@ -14,13 +16,18 @@ def load_ceda_test_data():
     This fixture ensures that the required test data repository
     has been cloned to the cache directory within the home directory.
     """
-    tmp_repo = "/tmp/.mini-ceda-archive"
-    test_data_dir = os.path.join(tmp_repo, "archive")
+    branch = "master"
+    target = os.path.join(MINI_ESGF_CACHE_DIR, branch)
 
-    if not os.path.isdir(MINI_CEDA_MASTER_DIR):
+    if not os.path.isdir(MINI_CEDA_CACHE_DIR):
+        os.makedirs(MINI_CEDA_CACHE_DIR)
 
-        os.makedirs(MINI_CEDA_MASTER_DIR)
-        os.system(f"git clone {CEDA_TEST_DATA_REPO_URL} {tmp_repo}")
+    if not os.path.isdir(target):
+        repo = Repo.clone_from(CEDA_TEST_DATA_REPO_URL, target)
+        repo.git.checkout(branch)
 
-        shutil.move(test_data_dir, MINI_CEDA_MASTER_DIR)
-        shutil.rmtree(tmp_repo)
+    elif os.environ.get("ROOCS_AUTO_UPDATE_TEST_DATA", "true").lower() != "false":
+        repo = Repo(target)
+        repo.git.checkout(branch)
+        repo.remotes[0].pull()
+
